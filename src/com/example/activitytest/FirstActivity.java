7855e5ac754dc4eda80f40a1d8657d9a4dcda461
Broccoli <453541724@qq.com>
2018-03-27 20:24:34 +0800

    chapter 2.2.4

--- src/com/example/activitytest/FirstActivity.java
+++ src/com/example/activitytest/FirstActivity.java
@@ -4,9 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class FirstActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.first_layout);
 	}
 }
