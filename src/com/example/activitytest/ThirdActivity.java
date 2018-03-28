9519a3d910921db0d7d5d76a5ee3916fc4482d2c
Broccoli <453541724@qq.com>
2018-03-28 11:14:48 +0800

    chapter 2.3 finished

---
+++ src/com/example/activitytest/ThirdActivity.java
@@ -0,0 +1,15 @@
+package com.example.activitytest;
+
+import android.app.Activity;
+import android.view.Window;
+import android.os.Bundle;
+
+public class ThirdActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.third_layout);
+	}
+}
